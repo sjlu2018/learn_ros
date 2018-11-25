2b2337b5b28203536bb299af41c9711f1b1645b8
lsj <599038521@qq.com>
2018-11-25 23:11:22 +0800

    add hole project to github

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/lu/md/lsj/code/ros/catkin_ws/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/lu/md/lsj/code/ros/catkin_ws/devel;/opt/ros/kinetic".split(';') if "/home/lu/md/lsj/code/ros/catkin_ws/devel;/opt/ros/kinetic" != "" else []
